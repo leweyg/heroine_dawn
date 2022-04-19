9120b1211db90836c16d73ad039bb9024640826a
Lewey Geselowitz <LeweyGeselowitz@gmail.com>
2022-04-18 17:09:52 -0700

    Topdown map guide visualizing

--- web/scripts/topdown_render.js
+++ web/scripts/topdown_render.js
@@ -1,6 +1,6 @@
 
 var dawnTopDownRenderer_prototype = {
-    createMapHtmlString : function(world,map_id) {
+    createMapHtmlString : function(world,map_id,callback_name) {
         var map = world.maps[map_id];
         var res = "<div style='position: relative; zoom:2; background-color: darkgray;";
         res += "width:" + (map.width * 16) + "px; height:" + (map.height * 16) + "px; ";
@@ -11,13 +11,30 @@ var dawnTopDownRenderer_prototype = {
                 if (tile_type == 0) continue;
                 var tile = world.tile_types[tile_type];
                 res += "<img src='web/" + tile.topdown_src + "' style='position: absolute;";
-                res += "left:" + (16 * x) + "px; top:" + (16 * y) + "px;"
-                res += "' />";
+                res += "left:" + (16 * x) + "px; top:" + (16 * y) + "px;";
+                if (!dawnTopDownRenderer.isThingAtXY(world,map_id,x,y)) {
+                    res += "opacity:50%;"
+                }
+                res += "' ";
+                if (callback_name) {
+                    res += "onclick='" + callback_name + "(" + map_id + "," + x + "," + y + ")' ";
+                }
+                res += " />";
             }
         }
         res += "</div>";
         return res;
     },
+    isThingAtXY : function(world,map_id,x,y) {
+        var map = world.maps[map_id];
+        for (var i in map.things) {
+            var thing = map.things[i];
+            if ((thing.x == x) && (thing.y == y)) {
+                return thing;
+            }
+        }
+        return null;
+    }
 };
 
 var dawnTopDownRenderer = new Object(dawnTopDownRenderer_prototype);
--- web/scripts/utils.js
+++ web/scripts/utils.js
@@ -17,6 +17,39 @@ var dawnUtils_prototype = {
         }
         rawFile.send(null);
     },
+    _removeQuote : "<rmq>",
+    customJsonStringify(obj) {
+        var str = JSON.stringify(obj, dawnUtils.customJsonReplacer, 2);
+        str = dawnUtils.replaceAll(str, "\"" + dawnUtils._removeQuote, "");
+        str = dawnUtils.replaceAll(str, dawnUtils._removeQuote + "\"", "");
+        return str;
+    },
+    customJsonReplacer : function(key,value) {
+        if (Array.isArray(value)) {
+            for (var i in value) {
+                if (typeof(value[i])!="number") {
+                    return value;
+                }
+            }
+            var res = "";
+            for (var i in value) {
+                var str = "" + value[i];
+                if (str.length < 2) {
+                    str = " " + str;
+                }
+                if (i != 0) res += ",";
+                res += str;
+            }
+            return dawnUtils._removeQuote + "[" + res + "]" + dawnUtils._removeQuote;
+        }
+        return value;
+    },
+    replaceAll : function (str, from, to) {
+        while (str.includes(from)) {
+            str = str.replace(from, to);
+        }
+        return str;
+    },
 };
 
 var dawnUtils = new Object(dawnUtils_prototype);
