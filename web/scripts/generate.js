7710738a43d173e6f56cece0a5a0eda7cda9ab02
Lewey Geselowitz <LeweyGeselowitz@gmail.com>
2022-12-11 19:41:50 -0800

    Seperated parts that can hide

--- web/scripts/generate.js
+++ web/scripts/generate.js
@@ -30,6 +30,21 @@ function exportMapToScene(map) {
             if (type.walkable) {
                 cell.walkable = true;
             }
+            if (type.parts_opt) {
+                var optParts = {
+                    name : "parts_opt",
+                    children : [],
+                };
+                cell.children.push(optParts);
+                cell.parts_opt = true;
+                for (var partIndex in type.parts_opt) {
+                    var path = type.parts_opt[partIndex];
+                    var p = {
+                        source : path
+                    };
+                    optParts.children.push(p);
+                }
+            }
             for (var partIndex in type.parts) {
                 var path = type.parts[partIndex];
                 var p = {
@@ -43,12 +58,15 @@ function exportMapToScene(map) {
     return scene;
 }
 
-for (var mapId in world.maps) {
-    var mapJson = exportMapToScene(world.maps[mapId]);
-    mapJson.metadata = {type:"lewcid_object"};
-    var mapText = JSON.stringify(mapJson,null,2);
-    fs.writeFileSync("web/models/map_" + mapId + ".json",mapText);
+function reExportMaps() {
+    for (var mapId in world.maps) {
+        var mapJson = exportMapToScene(world.maps[mapId]);
+        mapJson.metadata = {type:"lewcid_object"};
+        var mapText = JSON.stringify(mapJson,null,2);
+        fs.writeFileSync("web/models/map_" + mapId + ".json",mapText);
+    }
 }
+reExportMaps();
 
 
 console.log("Done.");
